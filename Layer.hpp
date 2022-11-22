08d40d1f956e9ab96323eba35ca7972deb2e11f2
Runpu Meng <112782995+runpumeng@users.noreply.github.com>
2022-11-22 00:55:37 -0500

    update:delete layer_sum (#14)

--- Layer.hpp
+++ Layer.hpp
@@ -11,7 +11,6 @@
 class Layer
 {
 public:
-    int layer_num;
     double xlen, ylen;
     std::vector<Obstacle> obs_list[6];
     double obs_x[12];        // The original point of the image window is the left bottum conner
@@ -19,11 +18,10 @@ public:
     double obs_width[12];    // now temporary design 2 layers' elements and each layer has 6 obstacles
     double obs_height[12];
     Layer();
-    void initialize_obs(void);
+    void initialize_obs(int current_layer);
 };
 Layer::Layer()
 {  
-   layer_num = 0;
    xlen = 1000.0;
    ylen = 1000.0;
    obs_x[12] = {125,875,500,125,680,310,700,300,600,350,850,550};
@@ -31,10 +29,10 @@ Layer::Layer()
    obs_width[12] = {250,250,200,175,220,235,200,100,300,250,300,100};
    obs_height[12] = {250,250,100,50,120,150,100,50,150,130,200,50};
 }
-void Layer::initialize_obs(void)
+void Layer::initialize_obs(int current_layer)
 {
     for(int i=0;i<sizeof(obs_list);i++)
     {
-        obs_list[i].Generate(obs_x[layer_num*6+i],obs_y[layer_num*6+i],obs_width[layer_num*6+i],obs_height[layer_num*6+i]);
+        obs_list[i].Generate(obs_x[current_layer*6+i],obs_y[current_layer*6+i],obs_width[current_layer*6+i],obs_height[current_layer*6+i]);
     }
 }
