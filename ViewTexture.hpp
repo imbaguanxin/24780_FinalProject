afd11c627ab02ba87ff8e3ba294354abe0689ac5
Xin Guan <xinguan@andrew.cmu.edu>
2022-11-30 18:07:16 -0500

    update: enabling texture.

---
+++ ViewTexture.hpp
@@ -0,0 +1,90 @@
+#ifndef ViewTexture_hpp
+#define ViewTexture_hpp
+
+#include "fssimplewindow.h"
+#include "yspng.h"
+#include "World.hpp"
+#include "Controller.hpp"
+
+#ifndef CONST_EM_PI
+#define CONST_EM_PI
+const double EM_PI = 3.1415926535;
+#endif
+
+class TextureData
+{
+public:
+    YsRawPngDecoder *decoders = nullptr;
+    GLuint *texIds = nullptr;
+    float *rgba = nullptr;
+    int size = 0;
+
+    TextureData(int length)
+    {
+        if (length <= 0)
+        {
+            size = 0;
+        }
+        else
+        {
+            size = length;
+        }
+        decoders = new YsRawPngDecoder[size];
+        texIds = new GLuint[size];
+        rgba = new float[4 * size];
+    }
+
+    ~TextureData()
+    {
+        if (nullptr == decoders)
+        {
+            delete[] decoders;
+        }
+        if (nullptr == texIds)
+        {
+            delete[] texIds;
+        }
+        if (nullptr == rgba)
+        {
+            delete[] rgba;
+        }
+    }
+
+    void setRGBA(float c[])
+    {
+        for (int i = 0; i < size * 4; ++i)
+        {
+            rgba[i] = c[i];
+        }
+    }
+};
+
+class ViewTexture
+{
+public:
+    World world;
+    int window_x_len, window_y_len;
+    TextureData *texData = nullptr;
+
+    ViewTexture();
+    ~ViewTexture();
+    void Cleanup();
+    // world to canvas
+    double W2CX(double x);
+    double W2CY(double y);
+    // rendering function
+    void DrawBackground(int texId);
+    void DrawObstacleOne(Obstacle &obs, int texId);
+    void DrawObstacles(void);
+    void DrawHero(void);
+    void DrawUI(void);
+    void Render(void);
+    // manipulating world
+    void Next(double time_interval, SpaceEvent s, HeroMoveDir dir, double intensity);
+    void InitTexture();
+
+protected:
+    void DrawQuadTex(double x[], double y[], int texId, double rgba[]);
+};
+
+#endif /* ViewTexture_hpp */
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "View.hpp"
 #include "fssimplewindow.h"
 #include "Controller.hpp"
+#include "ViewTexture.hpp"
+#include "yspng.h"
 
 #include <fstream>
 #include <iostream>
@@ -84,7 +86,80 @@ void CreateView(View4Test &v, std::string filename)
     v.world.current_layer = 0;
 }
 
-int main()
+void CreateViewTexture(ViewTexture &v, std::string filename)
+{
+    std::ifstream file;
+    std::string line, word;
+    double x, y, xlen, ylen, radius;
+    int layer = -1;
+    int total_layer = -1;
+    file.open(filename);
+
+    while (std::getline(file, line))
+    {
+        std::istringstream linestream(line);
+        linestream >> word;
+        if ("windowsize" == word)
+        {
+            linestream >> x;
+            linestream >> y;
+            v.window_x_len = int(x);
+            v.window_y_len = int(y);
+            printf("window size: x:%d, y:%d\n", v.window_x_len, v.window_y_len);
+        }
+        else if ("layersize" == word)
+        {
+            linestream >> x;
+            linestream >> y;
+            v.world.layer_x_len = x;
+            v.world.layer_y_len = y;
+            printf("layersize: x:%f, y:%f\n", v.world.layer_x_len, v.world.layer_y_len);
+        }
+        else if ("hero" == word)
+        {
+            linestream >> x;
+            linestream >> y;
+            linestream >> radius;
+            v.world.hero.x = x;
+            v.world.hero.y = y;
+            v.world.hero.radius = radius;
+            printf("hero: x:%f, y:%f, radius:%f\n", v.world.hero.radius, v.world.hero.y, radius);
+        }
+        else if ("totallayer" == word)
+        {
+            linestream >> total_layer;
+            for (int i = 0; i < total_layer; ++i)
+            {
+                Layer l;
+                v.world.layer_list.push_back(l);
+            }
+            printf("total layer: %lu\n", v.world.layer_list.size());
+        }
+        else if ("layer" == word)
+        {
+            linestream >> layer;
+            printf("layer: %d\n", layer);
+        }
+        else if ("obstacle" == word)
+        {
+            linestream >> x;
+            linestream >> y;
+            linestream >> xlen;
+            linestream >> ylen;
+            Obstacle obs(x, y, xlen, ylen, 0);
+            v.world.layer_list.at(layer).obs_list.push_back(obs);
+            printf("layer: %d, obstacle: %lf, %lf, %lf, %lf\n", layer, x, y, xlen, ylen);
+        }
+    }
+    file.close();
+    if (v.world.layer_list.size() != total_layer)
+    {
+        std::cout << "total layer and claimed layer not match" << std::endl;
+    }
+    v.world.current_layer = 0;
+}
+
+void main4Test()
 {
     FsChangeToProgramDir();
 
@@ -118,4 +193,54 @@ int main()
         FsSwapBuffers();
         FsSleep(20);
     }
+}
+
+void main4Texture()
+{
+    FsChangeToProgramDir();
+
+    ViewTexture v;
+
+    CreateViewTexture(v, "config.txt");
+    TextureData texD(1);
+    v.texData = &texD;
+    v.texData->decoders[0].Decode("grass.png");
+    float color[4] = {1.0, 1.0, 1.0, 1.0};
+    v.texData->setRGBA(color);
+
+    auto now = std::chrono::system_clock::now();
+
+    double time_interval = 0;
+    double intensity = 0;
+    HeroMoveDir dir = stand;
+    SpaceEvent se = spaceDefault;
+
+    FsOpenWindow(16, 16, v.window_x_len, v.window_y_len, 1);
+
+    Controller c;
+    while (!c.isGameEnd())
+    {
+        auto next_now = std::chrono::system_clock::now();
+        time_interval = double(std::chrono::duration_cast<std::chrono::milliseconds>(next_now - now).count()) / 1000;
+        now = next_now;
+
+        c.CheckKeyState();
+        dir = c.getMoveDir();
+        intensity = c.getIntensity();
+        se = c.getSpaceEvent();
+
+        v.Next(time_interval, se, dir, intensity);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+        v.Render();
+        FsSwapBuffers();
+        FsSleep(20);
+    }
+}
+
+int main()
+{
+    //    main4Test();
+    main4Texture();
+    return 0;
 }
